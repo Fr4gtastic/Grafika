ddd01beeb98e3af0b4993fe9d6db9f392b9ef497
Fr4gtastic <milosz.swierad@gmail.com>
2017-06-16 23:20:56 +0200

    działająca wersja

--- fromFile.h
+++ fromFile.h
@@ -1,29 +1,34 @@
-#pragma once
+#ifndef __FORMFILE_H__
+#define __FORMFILE_H__
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 
-void fromFile(std::ifstream & path, std::vector<std::vector<double>> & dataArray, std::ofstream & test1)
+void fromFile(std::ifstream & path, std::vector<std::vector<double> > & dataArray, std::ofstream & test1)
 {
-	
+//////// Czysci vector jezeli byl wczesniej zapelniony i ustala ilosc w nim zawartych zmiennych
+    for(int i=0; i<dataArray.size(); i++)dataArray[i].clear();
+    dataArray.clear();
+    dataArray.resize(4);
+    
+    int counter = 0;    // Zmienna pomocnicza uzywana w zapisywaniu danych    
+     double a;           // Zmienna przechowujaca przepisywane dane
 
-	// Wektor wektorów, przechowujący dane z pliku, sposób wypełnienia opisany niżej
-	//std::vector<std::vector<double>> dataArray;
-	dataArray.resize(4);
-	// Zmienna pomocnicza używana w zapisywaniu danych
-	int counter = 0;
-	// Zmienna przechowująca przepisywane dane
-	double a;
-
-	while (dataFile >> a)
-	{
-		// Zypełnianie wektorów danych na zasadzie: wektor 0: wartości x, wektor 1: wartości y, wektor 2: wartości z, wektor 3: wartości f(x, y, z)
-		dataArray[counter % 4].push_back(a);
-		counter++;
-	}
-
-	getchar();
-
-	// Zwraca wektor wektorów dataArray, jeśli będzie trzeba (np. dataArray będzie polem jakiejś klasy), można przerobić na void metodę klasy
-	return dataArray;
+    test1 << " ... zaczal wpsiywac   "; // uzupelnia log wczytywania // tzra potem usunac
+    
+///// Uzupelnianie wektorow danych na zasadzie: 
+///// wektor 0: wartosci x, wektor 1: wartosci y, wektor 2: wartosci z, wektor 3: wartosci f(x, y, z)
+    while(!path.eof())
+    {
+        path >> a;
+        test1 << a << " ";if(counter==3)test1<<std::endl;
+       
+        dataArray[counter].push_back(a);
+        counter=(counter+1)%4;
+    }
+    getchar();
 }
+
+#endif // __FORMFILE_H__
+
